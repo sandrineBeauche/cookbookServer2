789ee5f09c6f2d8d4cc3abae67c2f015a202d761
sandrine <sandrine.beauche@gmail.com>
2017-01-10 15:57:53 +0100

    fix some type incompatibilities

--- cookbook/NotFoundException.php
+++ cookbook/NotFoundException.php
@@ -4,7 +4,7 @@ namespace cookbook;
 
 class NotFoundException extends CookbookException {
     
-    public function __construct(string $itemName, int $itemId){
+    public function __construct($itemName, $itemId){
         parent::__construct(404, $itemName." with id ".$itemId." not found");
     }
 } 
