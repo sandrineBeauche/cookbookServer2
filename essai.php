19d5e96e56b015ae1b0f9eb755e0867559438d8c
sandrine <sandrine.beauche@gmail.com>
2017-02-05 22:28:35 +0100

    add openshift configuration files

--- essai.php
+++ essai.php
@@ -3,14 +3,13 @@
 // setup the autoloading
 require __DIR__ . '/vendor/autoload.php';
 
-//// setup Propel
-//require_once __DIR__ .'/conf/generated-conf/config.php';
-//
-//use cookbook\cookbook\UnitQuery as UnitQuery;
-//
-//$units = UnitQuery::create()->retrieveAll();
-//
-//echo "unités disponibles: ".json_encode($units);
+// setup Propel
+require_once __DIR__ .'/conf/config.php';
 
-echo "Hello world!";
+use cookbook\cookbook\UnitQuery as UnitQuery;
+
+$units = UnitQuery::create()->retrieveAll();
+
+echo "unités disponibles: ".json_encode($units);
+
 ?>
